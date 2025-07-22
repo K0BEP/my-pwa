c51c9827ca8c83bbe6418d351565277c044c54d5
Vsevolod Popov <popoffsewa@yandex.ru>
2025-07-22 23:02:02 +0300

    Update cache for all pages and images

--- sw.js
+++ sw.js
@@ -1,10 +1,52 @@
-const CACHE_NAME = 'my-pwa-cache-v1';
+const CACHE_NAME = 'stones-pwa-cache-v3';
 const urlsToCache = [
+  // Основные HTML-страницы
   '/',
   '/index.html',
-  '/styles.css',
-  '/script.js',
-  // добавьте другие ресурсы, которые нужно кэшировать
+  '/funny_stones.html',
+  '/sad_stones.html',
+  '/weird_stones.html',
+  '/feedback.html',
+  '/feedback_success.html',
+
+  // CSS и JS
+  '/style.css',
+  '/sw.js',
+
+  // Иконки PWA
+  '/icons/Icon192.png',
+  '/icons/icon512.png',
+
+  // Основные изображения
+  '/images/main_1.jpg',
+  '/images/main_2.jpg',
+  '/images/main_3.jpg',
+
+  // Веселые камни
+  '/images/funny_1.jpg',
+  '/images/funny_2.jpg',
+  '/images/funny_3.jpg',
+  '/images/funny_4.jpg',
+  '/images/funny_5.jpg',
+
+  // Грустные камни
+  '/images/sad_1.jpg',
+  '/images/sad_2.jpg',
+  '/images/sad_3.jpg',
+  '/images/sad_4.jpg',
+  '/images/sad_5.jpg',
+
+  // Странные камни
+  '/images/weird_1.jpg',
+  '/images/weird_2.jpg',
+  '/images/weird_3.jpg',
+  '/images/weird_4.jpg',
+  '/images/weird_5.jpg',
+
+  // PHP-скрипты (если нужно работать офлайн)
+  '/send_feedback.php',
+  '/feedback_success.php',
+  '/create_db.php'
 ];
 
 self.addEventListener('install', event => {
